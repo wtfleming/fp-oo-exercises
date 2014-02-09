feaf81b3624f782a2247b3ffc273be31c2fd9953
Will Fleming <wfleming77@gmail.com>
2014-02-08 17:29:48 -0800

    exercises for chapter 1

--- src/fp_oo/ch01.clj
+++ src/fp_oo/ch01.clj
@@ -1,10 +1,12 @@
 (ns fp-oo.ch01)
 
+;;; Exercise 1
 (defn exercise-1
   "Return second element of a list"
   [x]
   (nth x 1))
 
+;;; Exercise 2
 (defn exercise-2-1
   "Return third element of a list"
   [x]
@@ -14,3 +16,44 @@
   "Return third element of a list"
   [x]
   (first (rest (rest x))))
+
+;;; Exercise 3
+(defn add-squares
+  "Return the sum of the squares of numbers"
+  [& x]
+  (apply + (map #(* % %) x)))
+
+;;; Exercise 4
+(defn bizarre-factorial
+  "Factorial using neither recursion or iteration"
+  [x]
+  (apply * (range 1 (inc x))))
+
+(defn bizarre-factorial-2
+  "Factorial using neither recursion or iteration"
+  [x]
+  (reduce * (range 1 (inc x))))
+
+(defn factorial-recursion
+  [x]
+  (loop [x x
+         acc 1]
+    (if (= 0 x)
+      acc
+      (recur (dec x) (* x acc)))))
+
+;;; Exercise 6
+(defn prefix-of?
+  "Returns true if the elements in the candidate are the first elements in the sequence"
+  [candidate sequence]
+(= (take (count candidate) sequence) candidate))
+
+;;; Exercise 7
+(defn tails
+  "Returns a sequence of successively smaller subsequences of the argument"
+  [coll]
+  (let [inc-count-coll (inc (count coll))]
+    (map drop
+      (range inc-count-coll)
+      (repeat inc-count-coll coll))))
+
--- test/fp_oo/ch01_test.clj
+++ test/fp_oo/ch01_test.clj
@@ -14,3 +14,29 @@
 (deftest exercise-2-2-test
   (testing "returns the third element"
     (is (= (exercise-2-2 [0 1 2 3]) 2))))
+
+(deftest add-squares-test
+  (testing "add-squares"
+    (is (= (add-squares 1 2 5) 30))))
+
+(deftest bizarre-factorial-test
+  (testing "factorial"
+    (is (= (bizarre-factorial 5) 120))))
+
+(deftest bizarre-factorial-2-test
+  (testing "factorial"
+    (is (= (bizarre-factorial-2 5) 120))))
+
+(deftest factorial-recursion-test
+  (testing "factorial"
+    (is (= (factorial-recursion 5) 120))))
+
+(deftest prefix-of?-test
+  (testing "prefix-of?"
+    (is (= (prefix-of? [1 2] [1 2 3 4]) true))
+    (is (= (prefix-of? '(2 3) [1 2 3 4]) false))
+    (is (= (prefix-of? '(1 2) [1 2 3 4]) true))))
+
+(deftest tails-test
+  (testing "tails"
+    (is (= (tails '(1 2 3 4)) [[1 2 3 4] [2 3 4] [3 4] [4] []]))))
